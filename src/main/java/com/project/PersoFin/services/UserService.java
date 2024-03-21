66ee1d486c06e6a6021c921be3ecf659ff100c1f
Vaibhav Mistry <vaibhav.mistry@eviden.com>
2024-03-21 11:16:45 +0530

    reminder service added and made some other services in service layer

--- src/main/java/com/project/PersoFin/services/UserService.java
+++ src/main/java/com/project/PersoFin/services/UserService.java
@@ -3,28 +3,32 @@ package com.project.PersoFin.services;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.*;
-import com.project.PersoFin.daos.userDao;
 import org.springframework.stereotype.Service;
 
-
+import com.project.PersoFin.daos.UserDao;
 import com.project.PersoFin.models.user;
 
 @Service
 public class UserService {
 
-	@Autowired
-	public static userDao UserDao;
+	private final UserDao userDao;
 	
-	public static List<user> getallUsers(){
-		return UserDao.findAll();
+
+	@Autowired
+	public UserService(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public List<user> getallUsers(){
+		return userDao.findAll();
 	}
 	
 	public user registerUser(user User) {
-		return UserDao.save(User);
+		return userDao.save(User);
 	}
 	
 	public Optional<user> FindUser(int id) {
-		return UserDao.findById((long) id);
+		return userDao.findById((long) id);
 	}
 	
 	
